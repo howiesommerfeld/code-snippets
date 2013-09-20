430ff7f21a1355761698120f1ff211b05dabf4bc
Howie <Howie@10.0.0.3>
2013-09-20 11:42:29 +0200

    added a println

---
+++ CodeSnippets/src/codesnippets/CodeSnippets.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codesnippets;
+
+/**
+ *
+ * @author Howie
+ */
+public class CodeSnippets {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("do you see this proj bro??");
+        // TODO code application logic here
+    }
+}
